5139e73f485a9ea74464626994c8586437e4d5dd
nestor <nestorcolt@hotmail.com>
2020-11-21 00:33:03 +0100

    function working as lambda AWS

--- CatchFunction.cs
+++ CatchFunction.cs
@@ -1,32 +1,29 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using CatcherEngine.Modules;
+using Newtonsoft.Json.Linq;
 
 
 // The Main program for looking, catching and accepting blocks for the amazon flex service. Automate the process and handle a single user process instance and this needs
 // to be run per user request. (Ideally on a Lambda function over the AWS architecture)
 
+[assembly: Amazon.Lambda.Core.LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace CatcherEngine
 {
     class CatchFunction
     {
         public BlockCatcher Catcher = new BlockCatcher();
 
-        [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-        public async Task<string> CatchHandle(string userId, ILambdaContext context)
+        [Amazon.Lambda.Core.LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+        public async Task<string> CatchHandle(JObject userId, ILambdaContext context)
         {
-            try
-            {
-                await Catcher.GetOffersAsyncHandle(userId);
 
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            string user = userId.ToString();
+            HttpStatusCode responseCode = await Catcher.GetOffersAsyncHandle("4918");
 
-            return "My result here!";
+            return $"Response code {responseCode}";
 
         }
     }
