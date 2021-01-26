2904f7298bdac531b00e244aec6337ae68623ab1
nestor <nestorcolt@hotmail.com>
2021-01-26 15:59:08 +0100

    getBlocks

---
+++ Serverless/GetBlocks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+using SearchEngine.Modules;
+
+
+// The Main program for looking, catching and accepting blocks for the amazon flex service. Automate the process and handle a single user process instance and this needs
+// to be run per user request. (Ideally on a Lambda function over the AWS architecture)
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace SearchEngine.Serverless
+{
+    class GetBlocks
+    {
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+        public async Task<string> FunctionHandler(SNSEvent userData, ILambdaContext context)
+        {
+
+            //BlockCatcher catcher = new BlockCatcher(userData);
+            Console.WriteLine(userData.Records);
+            //HttpStatusCode responseCode = await catcher.GetOffersAsyncHandle(userData);
+            HttpStatusCode responseCode = HttpStatusCode.Accepted;
+            return responseCode.ToString();
+
+        }
+    }
+}
