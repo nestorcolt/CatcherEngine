c54c7c4cd7cc876b0f2c875a596b9dc0c762f6da
nestor <nestorcolt@hotmail.com>
2021-01-26 18:47:57 +0100

    auth user

---
+++ Serverless/AuthenticateUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+using SearchEngine.Properties;
+
+
+// The Main program for looking, catching and accepting blocks for the amazon flex service. Automate the process and handle a single user process instance and this needs
+// to be run per user request. (Ideally on a Lambda function over the AWS architecture)
+
+
+namespace SearchEngine.Serverless
+{
+    class AuthenticateUser
+    {
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+        public async Task<string> FunctionHandler(SNSEvent userData, ILambdaContext context)
+        {
+
+            foreach (var record in userData.Records)
+            {
+                var snsRecord = record.Sns;
+                Console.WriteLine($"[{record.EventSource} {snsRecord.Timestamp}] Message = {snsRecord.Message}");
+            }
+            HttpStatusCode responseCode = HttpStatusCode.Accepted;
+            return responseCode.ToString();
+
+        }
+    }
+}
