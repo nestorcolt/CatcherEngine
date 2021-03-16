dabd005b99e67c43b454cfd067433614d7e936c7
nestor <nestorcolt@hotmail.com>
2021-03-16 16:31:57 +0100

    update

--- Program.cs
+++ Program.cs
@@ -4,12 +4,10 @@ using System;
 namespace SearchEngine
 {
     class Program
-
     {
         static void Main(string[] args)
         {
-            string rt = "Atnr|EwICIAT6bN6fu3CAQv8IrNihJ-gdEUgyuCF8eJLx1wp4ZDk8Na7a8HGb4PGEte0VEQ1tCHaUnKPXyQn2CH03yV9o0YptXQx6dAt2CpZKy90gX0160--YtwiTtjVCoOQGcaP2R0le6nq9gMer2QeFQjrcL_WMgjlAcZTzfSlO_VlA_SUL4B2Jb_nDFWYqGmMeY7I1xTP-81z2oCZlr5c-YN_vh08VMcXtxRdwMegRfzCHoPTlqLq148aLzh0LBKbsbQ1d9YN6LYWv5S8i6C13YSVbHq75";
-            Authenticator.Authenticate(rt, "5").Wait();
+
         }
 
         public static DateTime SetTimeZone(DateTime timeToConvert, string timeZone)
