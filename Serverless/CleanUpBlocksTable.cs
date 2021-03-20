cad00c52ad2a745dd01ec412bedbfae5bfc787e4
nestor <nestorcolt@hotmail.com>
2021-03-20 11:57:24 +0100

    update

--- Serverless/CleanUpBlocksTable.cs
+++ Serverless/CleanUpBlocksTable.cs
@@ -3,15 +3,10 @@ using Amazon.Lambda.SNSEvents;
 using SearchEngine.Lib;
 using System.Threading.Tasks;
 
-
-// The Main program for looking, catching and accepting blocks for the amazon flex service. Automate the process and handle a single user process instance and this needs
-// to be run per user request. (Ideally on a Lambda function over the AWS architecture)
-
 namespace SearchEngine.Serverless
 {
     class CleanUpBlocksTable
     {
-
         [LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
         public async Task<string> FunctionHandler(SNSEvent snsEvent, ILambdaContext context)
         {
